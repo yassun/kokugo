0ec186479254f05c7c0c814a3c1b093e28b77144
Taga Yasunori <yasukun.taga@gmail.com>
2015-07-10 13:44:33 +0900

    Refactor

--- kokugo.go
+++ kokugo.go
@@ -7,7 +7,62 @@ import (
 )
 
 func Print(s string) {
+	matrix := newMatrix(s)
+	runes := matrix.rotate()
+	for i := range runes {
+		fmt.Println(string(runes[i]))
+	}
+}
 
+func newInputLine(s string) *inputLine {
+	return &inputLine{
+		lines: format(s),
+	}
+}
+
+type inputLine struct {
+	lines []string
+}
+
+func (in *inputLine) string2Runes() [][]rune {
+	var runes [][]rune
+	for i, s := range in.lines {
+		runes = append(runes, []rune{})
+		for _, r := range s {
+			runes[i] = append(runes[i], r)
+		}
+	}
+	return runes
+}
+
+func newMatrix(s string) *matrix {
+	inputLine := newInputLine(s)
+	return &matrix{
+		runes: inputLine.string2Runes(),
+	}
+}
+
+type matrix struct {
+	runes [][]rune
+}
+
+func (m *matrix) rotate() [][]rune {
+
+	runes := make([][]rune, len(m.runes[0]))
+	for i := range runes {
+		runes[i] = make([]rune, len(m.runes))
+	}
+
+	for y, _ := range m.runes {
+		for x, _ := range m.runes[0] {
+			runes[x][len(m.runes)-y-1] = m.runes[y][x]
+		}
+	}
+
+	return runes
+}
+
+func format(s string) []string {
 	lines := strings.Split(s, "\n")
 	maxLen := getMaxLen(lines)
 
@@ -20,13 +75,7 @@ func Print(s string) {
 		wrokLines[i] = line
 	}
 
-	wrokRunes := string2Runes(wrokLines)
-	runes := rotate(wrokRunes)
-
-	for i := range runes {
-		fmt.Println(string(runes[i]))
-	}
-
+	return wrokLines
 }
 
 func getMaxLen(lines []string) (maxLen int) {
@@ -36,30 +85,4 @@ func getMaxLen(lines []string) (maxLen int) {
 		}
 	}
 	return maxLen
-}
-
-func string2Runes(strings []string) [][]rune {
-	var runes [][]rune
-	for i, s := range strings {
-		runes = append(runes, []rune{})
-		for _, r := range s {
-			runes[i] = append(runes[i], r)
-		}
-	}
-	return runes
-}
-
-func rotate(stringRune [][]rune) [][]rune {
-	runes := make([][]rune, len(stringRune[0]))
-	for i := range runes {
-		runes[i] = make([]rune, len(stringRune))
-	}
-
-	for y, _ := range stringRune {
-		for x, _ := range stringRune[0] {
-			runes[x][len(stringRune)-y-1] = stringRune[y][x]
-		}
-	}
-
-	return runes
 }
